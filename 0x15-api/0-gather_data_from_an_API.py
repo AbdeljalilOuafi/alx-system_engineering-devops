8373e465b443a17a4f7cf130d26fc22effdeb94c
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-29 21:31:05 +0100

    style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
-"""
-0-gather_data_from_an_API Module
-
-This module fetches and displays information about an employee's TODO list progress
-using a REST API. Given an employee ID, it retrieves the employee's name and their
-tasks, indicating how many tasks have been completed and listing the titles of
-completed tasks.
-
-Usage:
-    ./0-gather_data_from_an_API.py <employee_id>
-"""
+"""0-gather_data_from_an_API Module"""
 if __name__ == "__main__":
     from sys import argv
     import requests
-
 
     if len(argv) == 2:
         try:
@@ -24,16 +13,16 @@ if __name__ == "__main__":
 
     # GET user info
 
-    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/\
+        {user_id}")
 
     if user.status_code == 200:
         user_data = user.json()
 
     # GET user task info
 
-
-    user_task = requests.get(f"https://jsonplaceholder.typicode.com/\
-        ?userId={user_id}")
+    user_task = requests.get(f"https://jsonplaceholder.typicode\
+        .com/?userId={user_id}")
 
     if user_task.status_code == 200:
         task_data = user_task.json()
@@ -46,8 +35,8 @@ if __name__ == "__main__":
         else:
             continue
 
-    print("Employee {} is done with tasks({}/{}):"
-        .format(user_data["name"], len(done_tasks), len(task_data)))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_data["name"], len(done_tasks), len(task_data)))
 
     for task in task_data:
         print("\t {}".format(task["title"]))
