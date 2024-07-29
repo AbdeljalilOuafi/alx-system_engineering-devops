008e3cf755ed79c4521de75579b02ff99a43e7ff
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-29 21:25:43 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,43 +10,44 @@ completed tasks.
 Usage:
     ./0-gather_data_from_an_API.py <employee_id>
 """
-from sys import argv
-import requests
+if __name__ == "__main__":
+    from sys import argv
+    import requests
 
 
-if len(argv) == 2:
-    try:
-        user_id = argv[1]
-        user_id = int(user_id)
-    except ValueError:
-        exit()
+    if len(argv) == 2:
+        try:
+            user_id = argv[1]
+            user_id = int(user_id)
+        except ValueError:
+            exit()
 
-# GET user info
+    # GET user info
 
-user = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
 
-if user.status_code == 200:
-    user_data = user.json()
+    if user.status_code == 200:
+        user_data = user.json()
 
-# GET user task info
+    # GET user task info
 
 
-user_task = requests.get(f"https://jsonplaceholder.typicode.com/\
-    ?userId={user_id}")
+    user_task = requests.get(f"https://jsonplaceholder.typicode.com/\
+        ?userId={user_id}")
 
-if user_task.status_code == 200:
-    task_data = user_task.json()
+    if user_task.status_code == 200:
+        task_data = user_task.json()
 
-done_tasks = []
+    done_tasks = []
 
-for task in task_data:
-    if task['completed']:
-        done_tasks.append(task)
-    else:
-        continue
+    for task in task_data:
+        if task['completed']:
+            done_tasks.append(task)
+        else:
+            continue
 
-print("Employee {} is done with tasks({}/{}):"
-      .format(user_data["name"], len(done_tasks), len(task_data)))
+    print("Employee {} is done with tasks({}/{}):"
+        .format(user_data["name"], len(done_tasks), len(task_data)))
 
-for task in task_data:
-    print("\t {}".format(task["title"]))
+    for task in task_data:
+        print("\t {}".format(task["title"]))
