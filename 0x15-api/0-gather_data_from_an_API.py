9659fa9e7cc610500330b5e69a12d9628a54e612
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-29 22:38:22 +0100

    completed_is_completed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,28 +12,16 @@ if __name__ == "__main__":
             exit()
 
     # GET user info
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id))
-
-    if user.status_code == 200:
-        user_data = user.json()
-    else:
-        print("Error occured fetching user")
-        print("Status code: {}".format(user.status_code))
-        exit()
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id)).json()
 
     # GET user task info
-    user_task = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={user_id}")
+    user_task = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={user_id}").json()
 
-    if user_task.status_code == 200:
-        task_data = user_task.json()
-    else:
-        print("Error occured fetching user task")
-        exit()
-
+    # GET completed tasks of the user
     done_tasks = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={user_id}&completed=true").json()
 
     print("Employee {} is done with tasks({}/{}):".format(
-        user_data.get("name"), len(done_tasks), len(task_data)))
+        user.get("name"), len(done_tasks), len(user_task)))
 
-    for task in task_data:
+    for task in user_task:
         print("\t "+task.get("title"))
