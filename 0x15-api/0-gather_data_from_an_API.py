8f39b8ef7dc6315e65d08c5a1e948f95e49b0881
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-29 22:39:50 +0100

    completed_is_completed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,13 +4,6 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-    if len(argv) == 2:
-        try:
-            user_id = argv[1]
-            user_id = int(user_id)
-        except ValueError:
-            exit()
-
     # GET user info
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id)).json()
 
