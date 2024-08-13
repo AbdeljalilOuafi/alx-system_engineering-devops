61c4e237163151293f1007ae26a40a47c0a4d032
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-08-13 14:28:30 +0100

    0-subs.py_doc

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
+"""This module returns the number of subs a subreddit has"""
 import requests
 
 def number_of_subscribers(subreddit):
+    """Get the number of subscribers for a subreddit."""
     response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
     if (response.status_code != 200):
         return 0
