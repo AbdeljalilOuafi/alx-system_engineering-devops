0a4bd8860455ea6aae06af300941ed878ff38977
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-08-13 14:35:31 +0100

    refactor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,9 @@ headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 def number_of_subscribers(subreddit):
     """Get the number of subscribers for a subreddit."""
     response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers)
-    if (response.status_code != 200):
-        return 0
-    result = response.json()
-    if "data" in result and "subscribers" in result["data"]:
-        return result["data"]["subscribers"]
+    if (response.status_code == 200):
+        result = response.json()
+        if "data" in result and "subscribers" in result["data"]:
+            return result["data"]["subscribers"]
     else:
         return 0
