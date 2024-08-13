bfd75eda63f19e55cfbe057ad9af655d17ff9109
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-08-13 17:38:09 +0100

    1-top_ten.py__doc__

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
+"""Top ten subreddit posts API Call"""
 import requests
 
 def top_ten(subreddit):
+    """Fetch top 10 posts from a subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=9".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Linux x86_64) Edge109.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
