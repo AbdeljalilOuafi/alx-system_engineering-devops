45d4a3d161ffe15e22b68409782a0081532607f4
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-08-13 21:58:49 +0100

    style

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,12 +3,15 @@
 """Fetch all hot pages for a subreddit"""
 import requests
 
+
 def recurse(subreddit, after=None, hot_list=None):
     if hot_list is None:
         hot_list = []
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+         AppleWebKit/537.36 (KHTML, like Gecko) \
+             Chrome/114.0.5735.199 Safari/537.36'}
     params = {'after': after}
     response = requests.get(url=url, headers=headers, params=params)
     if response.status_code != 200:
@@ -24,7 +27,3 @@ def recurse(subreddit, after=None, hot_list=None):
         return hot_list
 
     return recurse(subreddit, after, hot_list)
-
-
-
-
