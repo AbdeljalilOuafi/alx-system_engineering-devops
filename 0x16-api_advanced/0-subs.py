5fcaf090666fcd1b353373fecb2f4b92aed576cf
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-08-13 14:32:34 +0100

    add_user_agent_header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """This module returns the number of subs a subreddit has"""
 import requests
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 def number_of_subscribers(subreddit):
     """Get the number of subscribers for a subreddit."""
-    response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
+    response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers)
     if (response.status_code != 200):
         return 0
     result = response.json()
